0c9ccabfa53e5c6f0c662d8efbef29467b2b4a71
Jose Luis Martinez <jlmartinez@capside.com>
2018-04-24 23:19:12 +0200

    Some docu

--- lib/Kubernetes/CloudFormation/Worker.pm
+++ lib/Kubernetes/CloudFormation/Worker.pm
@@ -43,3 +43,31 @@ package Kubernetes::CloudFormation::Worker {
   __PACKAGE__->meta->make_immutable;
 }
 1;
+
+=head1 NAME
+
+Kubernetes::CloudFormation::Worker - Create kubernetes resources from your CloudFormation templates
+
+=head1 DESCRIPTION
+
+This is the implementation of the worker that creates, updates and deletes resources from a kubernetes cluster
+when commanded so from AWS CloudFormation.
+
+=head1 USAGE
+
+This class shouldn't be loaded directly. See the projects README for information about how to set up your kubernetes
+cluster and use CloudFormation to create resources in the cluster.
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright (c) 2018 by CAPSiDE
+
+This code is distributed under the Apache 2 License. The full text of the license can be found in the LICENSE file included with this module.
+
+=head1 AUTHORS
+
+  Jose Luis Martinez
+  JLMARTIN
+  jlmartinez@capside.com
+
+=cut
